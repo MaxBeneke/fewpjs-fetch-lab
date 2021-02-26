569e2c2e0b1d2d57df5e38a8b5575478bc92367a
Max Beneke <max.beneke@gmail.com>
2021-02-26 11:34:31 -0500

    commit tree 1db43f4345b0604020b2217055d305ca09c38362

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(books => {
+      return renderBooks(books);
+    })
 }
 
 function renderBooks(books) {
